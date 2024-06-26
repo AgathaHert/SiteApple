41365327aeeec17873a0f085acad0f83d107dbc8
AgathaHert <agatha38hertel@gmail.com>
2024-06-26 14:08:01 -0300

    Primeiro Commit - Site Apple

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function changeColor(color) {
+  let circulo = document.querySelector(".circulo-iphone");
+  circulo.style.background = color;
+}
+
+function changeImage(img) {
+  let iphone = document.querySelector(".iphone");
+  iphone.src = img;
+}
